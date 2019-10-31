d4da47be60a10cc84205eb1c528736e2aacbd826
Gustav Moorhouse <g.moorhouse@me.com>
2019-10-31 11:12:53 +0100

    added nav

---
+++ nav.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8" />
+	<title>LWW &#8211; precision barista tools</title>
+	<link rel='stylesheet' href='style.css' type='text/css'/>
+</head>
+
+<body>
+	<header class="header">
+	</header>
+	
+	<nav class="handheld">
+		<?php include ?>
+	</nav>
+	
+	<nav class="desktop">
+	</nav>
+	
+<div class="page-wrapper">
+<header id="masthead" class="site-header" role="banner">
+
+<div class="hgroup full-container ">
+
+<a href="https://lynweber.com/" title="LWW" rel="home" class="logo"><img src="https://lynweber.com/wp-content/uploads/2015/08/lww-logo_v4.png" class="logo-height-constrain" height="75" alt="LWW Logo"</a>
+			
+<div class="support-text"></div>
+
+</div><!-- .hgroup.full-container -->
+
+	
+<nav role="navigation" class="site-navigation main-navigation primary use-sticky-menu mobile-navigation">
+
+	<div class="full-container">
+							<div id="search-icon">
+				<div id="search-icon-icon"><div class="vantage-icon-search"></div></div>
+				
+<form method="get" class="searchform" action="https://lynweber.com/" role="search">
+	<input type="text" class="field" name="s" value="" placeholder="Search"/>
+</form>
+			</div>
+			
+home
+about
+products
+  moulin
+  bean counter
+  eg1
+  hg1
+  accessories
+    bean cellars
+      glass
+      commercial
+    blind shaker
+    blond tumblr
+    looking glass
+    basemat
+    steam dial
+news
+support
+account
+  orders
+  register
+  contact
+  log in
+store
+  cart
+  checkout
+  waitlist
+  asia
+  
+	</div><!-- #main .site-main -->
+
+	
+	
+	<footer id="colophon" class="site-footer" role="contentinfo">
+
+			<div id="footer-widgets" class="full-container">
+			<aside id="text-3" class="widget widget_text">			<div class="textwidget"> <p align="right">
+
+ <a href="http://lynweber.com/terms-conditions/">Terms & Conditions</a> 
+
+</p> </div>
+		</aside>		</div><!-- #footer-widgets -->
+	
+			<div id="site-info">
+			&copy; LWW, 2019		</div><!-- #site-info -->
+	
+	
+</footer><!-- #colophon .site-footer -->
+
+	
+</div><!-- #page-wrapper -->
+</body>
+</html>
